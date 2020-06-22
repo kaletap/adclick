b49d06c8cc85d74d1780052fae8483d30aa51111
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-06-22 14:50:02 +0200

    Added static vocabulary in data preprocessing.

--- preprocessing/create_vocab.py
+++ preprocessing/create_vocab.py
@@ -12,7 +12,7 @@ From a data description (https://www.kaggle.com/c/kddcup2012-track2/overview):
  Tokens are delimited by the character ‘|’. 
 Because of that, since hash of the same word is always the same, I assume that the same ID in different columns
 represent the same word.
-On 70 mln row file, the script takes approximately 23 minutes to run.
+the script runs approximately 20k iterations (rows) per second.
 """
 
 parser = argparse.ArgumentParser()
@@ -23,29 +23,31 @@ parser.add_argument("-c", "--columns",
                     default="AdKeyword_tokens,AdTitle_tokens,AdDescription_tokens,Query_tokens",
                     help="Text column from which to create vocabulary. "
                          "Comma separated: for example 'AdTitle_tokens,Query_tokens")
-parser.add_argument("-v", "--vocab_file", type=str, default="data/vocab.txt")
-parser.add_argument("-wc", "--word_count_file", type=str, default="data/word_count.json")
+# parser.add_argument("-v", "--vocab_file", type=str, default="data/vocab.txt")
+# parser.add_argument("-wc", "--word_count_file", type=str, default="data/word_count.json")
 
 args = parser.parse_args()
 print(args)
 
-word_counter = Counter()
-
 with open(args.filename, newline="") as csv_file:
     reader = csv.reader(csv_file, delimiter="\t")
     columns = next(reader)
+    word_counter = {column: Counter() for column in args.columns}
     # Figure out position of each chosen column
     column_position = {column: position for position, column in enumerate(columns) if column in args.columns}
     for row in tqdm(reader):
         for column, position in column_position.items():
             tokens = row[position].split("|")
-            word_counter.update(tokens)
-    # Sort counter by most popular words
-    word_count_list = sorted(word_counter.items(), key=lambda item: item[1], reverse=True)
-    print("Total number of words", len(word_count_list))
-    print("Ten most popular words and their count: ", *word_count_list[:10])
-    words = [word + "\n" for word, _ in word_count_list]
-    with open(args.vocab_file, "w") as vocab_file:
-        vocab_file.writelines(words)
-    with open(args.word_count_file, "w") as word_count_file:
-        json.dump(OrderedDict(word_count_list), word_count_file)  # json will be ordered by count
+            word_counter[column].update(tokens)
+    for column in args.columns:
+        print(column)
+        # Sort counter by most popular words
+        word_count_list = sorted(word_counter[column].items(), key=lambda item: item[1], reverse=True)
+        print("Total number of words", len(word_count_list))
+        print("Ten most popular words and their count: ", *word_count_list[:10])
+        words = ["<PAD>\n"] + [word + "\n" for word, _ in word_count_list]
+        with open("data/vocab/{}.txt".format(column), "w") as vocab_file:
+            vocab_file.writelines(words)
+        with open("data/vocab/word_count_{}.txt".format(column), "w") as word_count_file:
+            json.dump(OrderedDict(word_count_list), word_count_file)  # json will be ordered by count
+        print()
--- preprocessing/tokens.py
+++ preprocessing/tokens.py
@@ -9,22 +9,64 @@ class Vocabulary:
         """
         words = ["<PAD>", "<UNK>"]  # pad and unknown token, represented with 0 and 1 respectively
         with open(vocab_file) as f:
-            for _ in range(vocab_size):
-                words.append(next(f).strip())
+            try:
+                for i in range(vocab_size):
+                    words.append(next(f).strip())
+            except StopIteration:  # TODO: should I maybe write it better?
+                raise StopIteration("Vocabulary file contains only {} lines".format(i))
         self.word2idx = {word: num for num, word in enumerate(words)}
         self.idx2word = words
 
+        table = tf.lookup.StaticHashTable(
+            initializer=tf.lookup.KeyValueTensorInitializer(
+                keys=tf.constant([0, 1, 2, 3]),
+                values=tf.constant([10, 11, 12, 13]),
+            ),
+            default_value=tf.constant(-1),
+            name="class_weight"
+        )
+
+    def get_word_num(self, word):
+        print(word.dtype)
+        print(word.numpy())
+        return self.word2idx.get(word.numpy(), 1)
+
     def __getitem__(self, word):
-        print(tf.executing_eagerly())
         if isinstance(word, tf.Tensor):
-            word = word.numpy()
-        return self.word2idx[word]
+            return tf.py_function(func=self.get_word_num, inp=[word], Tout=tf.int32, name="word2idx")
+        else:
+            return self.word2idx[word]
 
     def __call__(self, word):
         return self[word]
 
 
 # Sequence of tokens transformer
+class TokenizeWithVocabFile:
+    def __init__(self, name: str, vocab_size: int, max_length: int, vocab_file: str, oov_buckets: int = 10):
+        self.name = name
+        self.vocab_size = vocab_size
+        self.max_length = max_length
+        self.table = tf.lookup.StaticVocabularyTable(
+            tf.lookup.TextFileInitializer(vocab_file,
+                                          tf.string,
+                                          tf.lookup.TextFileIndex.WHOLE_LINE,
+                                          tf.int64,
+                                          tf.lookup.TextFileIndex.LINE_NUMBER,
+                                          delimiter=" ",
+                                          vocab_size=vocab_size),
+            num_oov_buckets=oov_buckets
+        )
+
+    def __call__(self, features, labels):
+        tokens_list = features[self.name]
+        tokens_list = tf.strings.split(tokens_list, sep="|").to_tensor(default_value="<PAD>", shape=[None, self.max_length])
+        tokens = self.table.lookup(tokens_list)
+        # tokens = tf.strings.to_hash_bucket_fast(tokens_list, num_buckets=self.vocab_size)
+        features[self.name] = tokens
+        return features, labels
+
+
 class Tokenize:
     def __init__(self, name: str, vocab_size: int, max_length: int):
         self.name = name
