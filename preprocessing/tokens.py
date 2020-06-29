70142b2e7759bee47f487a9d0e918a7b194b58bd
Przemysław Kaleta <przemek2889@gmail.com>
2020-06-29 13:35:06 +0200

    Getting rid of Vocabulary class in tokens.py.

--- preprocessing/tokens.py
+++ preprocessing/tokens.py
@@ -1,46 +1,6 @@
 import tensorflow as tf
 
 
-class Vocabulary:
-    def __init__(self, vocab_file: str, vocab_size: int):
-        """
-        :param vocab_file: file with vocabulary ordered by popularity of a word
-        :param vocab_size: maximum size of a vocabulary (we use top vocab_size words of vocab_file)
-        """
-        words = ["<PAD>", "<UNK>"]  # pad and unknown token, represented with 0 and 1 respectively
-        with open(vocab_file) as f:
-            try:
-                for i in range(vocab_size):
-                    words.append(next(f).strip())
-            except StopIteration:  # TODO: should I maybe write it better?
-                raise StopIteration("Vocabulary file contains only {} lines".format(i))
-        self.word2idx = {word: num for num, word in enumerate(words)}
-        self.idx2word = words
-
-        table = tf.lookup.StaticHashTable(
-            initializer=tf.lookup.KeyValueTensorInitializer(
-                keys=tf.constant([0, 1, 2, 3]),
-                values=tf.constant([10, 11, 12, 13]),
-            ),
-            default_value=tf.constant(-1),
-            name="class_weight"
-        )
-
-    def get_word_num(self, word):
-        print(word.dtype)
-        print(word.numpy())
-        return self.word2idx.get(word.numpy(), 1)
-
-    def __getitem__(self, word):
-        if isinstance(word, tf.Tensor):
-            return tf.py_function(func=self.get_word_num, inp=[word], Tout=tf.int32, name="word2idx")
-        else:
-            return self.word2idx[word]
-
-    def __call__(self, word):
-        return self[word]
-
-
 # Sequence of tokens transformer
 class TokenizeWithVocabFile:
     def __init__(self, name: str, vocab_size: int, max_length: int, vocab_file: str, oov_buckets: int = 10):
